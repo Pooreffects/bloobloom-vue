5de38d9fc314a159565335b240c52f95953c5619
Mohamed Benabdellah <pooreffects@gmail.com>
2023-01-18 16:15:43 +0000

    feat: fetched and rendered collection specific data based on a global store, yet to handle loading and error states, and fetch data due to route params, filters queries, and Menu routing

---
+++ src/components/Loader.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>lOADING....! 👀</div>
+</template>
+
+<script setup></script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,29 +1,73 @@
 <template>
+  <!-- Work and manipulate the menu and extracting it into its own component (prolly dump GSAP too) -->
   <header
-    class="py-6 px-6 w-full flex items-center justify-between border-b-[1px] border-slate-800"
+    class="p-0 w-full flex items-center justify-between border-b-[1px] border-slate-800"
   >
     <a
-      v-on:mouseover="isHovered"
-      v-on:mouseleave="notHovered"
-      class="text-base font-style font-normal tracking-wider hover:underline hover:underline-offset-4 hover:cursor-pointer"
-      >MENU</a
+      @mouseover="
+        () => {
+          openMenu();
+          menu = true;
+        }
+      "
+      @mouseleave="
+        () => {
+          closeMenu();
+          menu = false;
+        }
+      "
+      class="px-10 m-0 p-4 text-base font-style font-normal tracking-wider hover:underline hover:underline-offset-4 hover:cursor-pointer"
     >
+      MENU
+    </a>
 
-    <a href="/">
+    <a class="pr-6" href="/">
       <img src="../assets/bloobloom.png" alt="Logo" />
     </a>
   </header>
+  <aside
+    v-if="menu"
+    ref="sidebar"
+    class="fixed left-0 flex flex-col w-80 min-h-screen overflow-hidden p-4 bg-slate-200 z-50"
+  >
+    Sidebar
+  </aside>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
+import gsap from 'gsap';
 
-export default {
-  name: 'TheHeader',
-  setup() {
-    /* track Menu link on hover */
-    const menuState = ref('closed');
-    return { menuState };
-  },
-};
+const menu = ref(false);
+const sidebar = ref(null);
+
+const tl = gsap.timeline();
+
+function openMenu() {
+  tl.fromTo(
+    sidebar.value,
+    {
+      x: -380,
+    },
+    {
+      x: 0,
+      duration: 1,
+      ease: 'expo.out',
+    }
+  );
+}
+
+function closeMenu() {
+  tl.fromTo(
+    sidebar.value,
+    {
+      x: 0,
+    },
+    {
+      x: -384,
+      duration: 1,
+      ease: 'ease.out',
+    }
+  );
+}
 </script>
